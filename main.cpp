85b853af03e8f5422703d43d464cfbf9d8b55e4b
Ali Akbar Shahi <ali.akbar.shahi.junior@gmail.com>
2020-01-06 17:01:33 +0330

    nothing

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 #include "printboard.h"
 #include "fillboard.h"
 
-
 int main()
 {
     char board[24][40]={};
     fillboard(board);
     printBoard(board);
+    cout<<endl;
 
     return 0;
 }
