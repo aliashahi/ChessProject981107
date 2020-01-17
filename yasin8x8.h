fbca0bef37bb477d790a5436ecd5d237915b5650
yasin <yasinghoreshi96@gmail.com>
2020-01-17 18:51:31 +0330

    moshkel pak nakardan jaye ghabli hal shod

--- yasin8x8.h
+++ yasin8x8.h
@@ -6,11 +6,15 @@ void working8x8(char board[24][40],char workboard[8][8])
         col=2;
         for(j=0;j<8;j++)
         {
-            if(workboard[i][j]!=' ')
             board[row][col]=workboard[i][j];
-            else if(workboard[i][j]==' ')
+            if(workboard[i][j]==' ')
             {
                 if((i%2==0)&&(j%2==0))
+                 board[row][col]=(char)219u;
+            }
+            if(workboard[i][j]==' ')
+            {
+                if((i%2==1)&&(j%2==1))
                  board[row][col]=(char)219u;
             }
             col=col+5;
